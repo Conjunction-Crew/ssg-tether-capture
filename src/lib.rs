6728d76c0ae063ec037d607751417581dbe60bdd
Blake Owen <99156569+odevop@users.noreply.github.com>
2026-02-11 15:26:47 -0800

    SCRUM-10: Rigid Body Plugin Selection and Setup

---
+++ src/lib.rs
@@ -0,0 +1,44 @@
+use avian3d::prelude::*;
+use bevy::prelude::*;
+
+// Application plugin and system setup
+pub fn run() {
+    let mut app = App::new();
+    app.add_plugins(DefaultPlugins.build().disable::<TransformPlugin>())
+    .add_plugins(PhysicsPlugins::default())
+    .run();
+}
+
+// Minimal test app harness for unit testing
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn test_app() -> App {
+        let mut app = App::new();
+        app.add_plugins((
+            MinimalPlugins,
+            TransformPlugin,
+            ColliderHierarchyPlugin,
+            AssetPlugin::default(),
+        ));
+        app.init_asset::<Mesh>();
+
+        app
+    }
+
+    #[test]
+    fn minimal_rigidbody_setup() {
+        let mut app = test_app();
+
+        let test_sphere_mesh = Mesh::from(Sphere::new(1.0));
+
+        app.world_mut().spawn((
+            RigidBody::Dynamic,
+            Collider::convex_hull_from_mesh(&test_sphere_mesh).unwrap(),
+            Transform::from_xyz(0.0, 4.0, 0.0),
+        ));
+
+        app.update();
+    }
+}
