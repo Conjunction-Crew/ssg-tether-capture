1e6d804dc74e8c0aab081028e1183b8f271a9953
Blake <epicbwo@gmail.com>
2026-02-11 14:51:44 -0800

    test app harness

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,10 @@ use avian3d::prelude::*;
 use bevy::prelude::*;
 
 pub fn run() {
-    create_app().add_plugins(DefaultPlugins.build()).run();
-}
-
-pub fn create_app() -> App {
-    let app = App::new();
-    app
+    let mut app = App::new();
+    app.add_plugins(DefaultPlugins.build().disable::<TransformPlugin>())
+    .add_plugins(PhysicsPlugins::default())
+    .run();
 }
 
 // Integration Tests
@@ -15,10 +13,31 @@ pub fn create_app() -> App {
 mod tests {
     use super::*;
 
+    fn test_app() -> App {
+        let mut app = App::new();
+        app.add_plugins((
+            MinimalPlugins,
+            TransformPlugin,
+            ColliderHierarchyPlugin,
+            AssetPlugin::default(),
+        ));
+        app.init_asset::<Mesh>();
+
+        app
+    }
+
     #[test]
-    fn minimal_setup() {
-        let mut app = create_app();
-        app.add_plugins(MinimalPlugins.build());
+    fn minimal_rigidbody_setup() {
+        let mut app = test_app();
+
+        let test_sphere_mesh = Mesh::from(Sphere::new(1.0));
+
+        app.world_mut().spawn((
+            RigidBody::Dynamic,
+            Collider::convex_hull_from_mesh(&test_sphere_mesh).unwrap(),
+            Transform::from_xyz(0.0, 4.0, 0.0),
+        ));
+
         app.update();
     }
 }
